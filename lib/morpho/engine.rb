afc248b8aae4289c1301085f0069da027cb7196f
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-09-20 09:38:20 -0600

    auto load migrations on migrate

--- lib/morpho/engine.rb
+++ lib/morpho/engine.rb
@@ -4,8 +4,10 @@ module Morpho
     engine_name 'morpho'
 
     initializer 'morpho', before: :load_config_initializers do |app|
+    initializer 'morpho.migrations', before: :load_config_initializers do |app|
       config.paths['db/migrate'].expanded.each do |expanded_path|
         Rails.application.config.paths['db/migrate'] << expanded_path
+        app.config.paths['db/migrate'] << expanded_path
       end
     end
   end
