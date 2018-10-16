969ec33c9a8d1a438428919db73d8f8a33659cb8
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-10-16 13:24:21 -0600

    test dummy api base class and routes mount added

---
+++ test/dummy/app/api/morpho/api.rb
@@ -0,0 +1,24 @@
+module Morpho
+  class API < ::Grape::API
+    format :json
+    rescue_from :all
+
+    mount Morpho::Resources::Users
+    mount Morpho::Resources::Tokens
+    mount Morpho::Resources::Passwords
+    mount Morpho::Resources::Unlocks
+    mount Morpho::Resources::Activations
+
+    add_swagger_documentation({
+      info: {
+        title: Morpho.config.api.title,
+        description: Morpho.config.api.description,
+        models: Morpho.config.api.entities
+      }
+    })
+
+    route :any, '*path' do
+      error!({ message: I18n.t('morpho.api.messages.not_found') }, 404)
+    end
+  end
+end
