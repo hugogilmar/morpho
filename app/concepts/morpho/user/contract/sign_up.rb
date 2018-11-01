479ce2f37c03c2db8b77169671502b2ccc39b682
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-10-31 19:00:25 -0600

    ✨ Include password format validations

--- app/concepts/morpho/user/contract/sign_up.rb
+++ app/concepts/morpho/user/contract/sign_up.rb
@@ -7,6 +7,16 @@ module Morpho
     property :email
     property :password
     property :password_confirmation, virtual: true
+    validates :email, presence: true, unique: true, email_format: true
+    validates :password,
+              presence: true,
+              confirmation: true,
+              length: { minimum: Morpho.config.auth.password_minimum_length },
+              # rubocop:disable Style/HashSyntax
+              :'morpho/validators/contain_number' => true,
+              :'morpho/validators/contain_uppercase' => true,
+              :'morpho/validators/contain_symbol' => true
+    # rubocop:enable Style/HashSyntax
     validates :password_confirmation, presence: true
   end
 end
